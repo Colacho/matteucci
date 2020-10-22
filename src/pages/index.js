848731835ad2431e5094632e608b7ab0db212c72
Colacho <fcolacilli@gmail.com>
2020-10-22 20:30:02 -0300

    tailwind update funcionando

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ console.log('asdlasdl');
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1 className="bg-blue-500 text-white">Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
