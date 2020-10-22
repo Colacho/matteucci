a0c5f462962c63d6a7010b8f08e652dd8340cb8f
Colacho <fcolacilli@gmail.com>
2020-10-22 19:50:16 -0300

    prettier format

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-console.log('asdlmsdñlf asñkl,a ck')
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
+console.log('asdlasdl');
 
 const IndexPage = () => (
   <Layout>
@@ -18,6 +18,6 @@ const IndexPage = () => (
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
