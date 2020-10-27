1af08e43fa426d768b1ef56bfd2109a652d2af3e
Colacho <fcolacilli@gmail.com>
2020-10-26 21:09:45 -0300

    VideoCard Workig

--- src/components/videocard.js
+++ src/components/videocard.js
@@ -1,42 +1,19 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
 
 import '../styles/index.css';
 import './layout.css';
 import Img from 'gatsby-image';
-import SEO from '../components/seo';
 
-const videocard = ({ data }) => {
+const VideoCard = ({ image }) => {
+  console.log(image);
   return (
-    <div id="vidCont">
-      <Img fluid={data?.item01?.childImageSharp?.fluid} />
+    <div>
+      <Img fluid={image.childImageSharp.fluid} />
       <p>Info sobre video</p>
     </div>
   );
 };
 
-export const query = graphql`
-  query {
-    item01: file(relativePath: { eq: "bonitasChico.jpg" }) {
-      childImageSharp {
-        # Specify the image processing specifications right in the query.
-        # Makes it trivial to update as your page's design changes.
-        fluid(maxWidth: 500) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    item02: file(relativePath: { eq: "jr.jpg" }) {
-      childImageSharp {
-        # Specify the image processing specifications right in the query.
-        # Makes it trivial to update as your page's design changes.
-        fluid(maxWidth: 500) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-  }
-`;
-
-export default videocard;
+export default VideoCard;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,21 +4,46 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import Image from '../components/image';
 import SEO from '../components/seo';
-import Videocard from '../components/videocard';
+import VideoCard from '../components/videocard';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1 className="bg-blue-500 text-white">Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Videocard />
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-);
+const IndexPage = ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1 className="bg-blue-500 text-white">Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      {data && <VideoCard image={data.item01} />}
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query {
+    item01: file(relativePath: { eq: "bonitasChico.jpg" }) {
+      childImageSharp {
+        # Specify the image processing specifications right in the query.
+        # Makes it trivial to update as your page's design changes.
+        fluid(maxWidth: 500) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    item02: file(relativePath: { eq: "jr.jpg" }) {
+      childImageSharp {
+        # Specify the image processing specifications right in the query.
+        # Makes it trivial to update as your page's design changes.
+        fluid(maxWidth: 500) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
 
 export default IndexPage;
