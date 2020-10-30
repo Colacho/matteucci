f6975d2ef35897ca5ff77f96903211151e97fb67
Colacho <fcolacilli@gmail.com>
2020-10-30 16:42:16 -0300

    saving consolelog

--- src/components/videocard.js
+++ src/components/videocard.js
@@ -7,7 +7,7 @@ import './layout.css';
 import CardData from './carddata';
 import CardInfo from './cardinfo';
 //import Img from 'gatsby-image';
-
+console.log('colacho');
 const VideoCard = ({ data }) => {
   const cardList = CardData.map((i) => (
     <div
