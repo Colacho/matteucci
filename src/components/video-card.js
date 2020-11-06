021e30a9847c44a3a64f131ea1b20d08e99585f8
Colacho <fcolacilli@gmail.com>
2020-11-05 21:40:27 -0300

    video card style

--- src/components/video-card.js
+++ src/components/video-card.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import T from 'prop-types';
 import { Link } from 'gatsby';
+import Video from './video';
 
 const VideoCard = ({ videoId, image, videoSrcURL, gender, duration, location, direction }) => {
   return (
     <div>
-      <div className="hover:opacity-50">
-        <Link to={`/video/${videoId}`} state={{ vidLoc: videoSrcURL }}>
-          {image}
-        </Link>
+      <div className="absolute m-20">
+        <div className="border-solid border-4 border-yellow-500 bg-yellow-500 rounded h-auto w-auto text-center rounded-full ">
+          Ver
+        </div>
       </div>
-      <div>
+      <div className="hover:opacity-50 flex-1 h-64">
+        <Link to={`/video/${videoId}`}>{image}</Link>
+      </div>
+      <div className=" mt-8 h-auto">
         <p>Genero: {gender}</p>
         <p>Dirección: {direction}</p>
         <p>Duración: {duration}</p>
