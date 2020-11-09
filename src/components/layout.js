6f7db611c0cae4e882e190a20a6ffc203f172185
Colacho <fcolacilli@gmail.com>
2020-11-09 19:10:14 -0300

    no sirve

--- src/components/layout.js
+++ src/components/layout.js
@@ -28,35 +28,18 @@ const Layout = ({ children }) => {
 
   return (
     <>
-<<<<<<< HEAD
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-      // style={{
-      //   margin: `0 auto`,
-      //   maxWidth: 960,
-      //   padding: `0 1.0875rem 1.45rem`,
-      // }}
-      >
-=======
       <Header siteTitle={data.site.siteMetadata?.title || 'Title'} />
       <Helmet>
         <body className="bg-primary text-primary-contrast font-roboto"></body>
       </Helmet>
       <div className="container mx-auto">
->>>>>>> c73aeb0ec9df03999a30981d9a39dbcc1fe5150c
         <main>{children}</main>
         <footer
           style={{
             marginTop: `2rem`,
           }}
         >
-<<<<<<< HEAD
-          <Footer />© {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-=======
           © {new Date().getFullYear()}, Built with <a href="https://www.gatsbyjs.com">Gatsby</a>
->>>>>>> c73aeb0ec9df03999a30981d9a39dbcc1fe5150c
         </footer>
       </div>
     </>
