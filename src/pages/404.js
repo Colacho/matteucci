1a8c0f5bd9d5b019e200a67f18611b3ffa5cc25f
Colacho <fcolacilli@gmail.com>
2020-12-02 10:35:35 -0300

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,22 @@
-import React from "react"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import React from 'react';
+import BlackFringe from '@/components/black-fringe';
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <BlackFringe title={'404: Not Found'}>
+      <h5>Pagina No disponible</h5>
+      <p>
+        Quiza te interese visitar nuestra pagina de{' '}
+        <Link to="/titles/" className="underline text-accent-500">
+          títulos
+        </Link>
+      </p>
+    </BlackFringe>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
